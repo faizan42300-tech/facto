1c7af5bcee4dc631a613a5b69067a3b61b7fc6b5
Mohd Mehdi Miyan <faizan42300@gmail.com>
2025-12-19 13:36:25 +0530

    Fix formatting of factorial calculation

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
 def fact(n):                                                #3  -flow of data-
    if (n==1 or n==0):                                       #4          #6
     return 1                                                #4 if true  #6
-   return n*fact(n-1)  #factorial formula = n*fact(n-1)     #5         #7     so on.....
+   return n*fact(n-1)                                       #5         #7     so on.....
 n=int(input("enter a number:"))                             #1         
 print(f"The factorial of {n} is:{fact(n)}")                 #2    
